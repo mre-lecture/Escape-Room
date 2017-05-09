385b388064b7e2948529e9d70c966f8dbea77d71
Patrick <patrickgeyer94@gmx.de>
2017-05-09 22:12:44 +0200

    add multiplayer test stuff

---
+++ Escape Room/Assets/Escape Room/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+    public GameObject playerPrefab;
+
+    // Use this for initialization
+    void Start () {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private const string roomName = "RoomName";
+    private RoomInfo[] roomsList;
+
+    void OnGUI()
+    {
+        if (!PhotonNetwork.connected)
+        {
+            GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        }
+        else if (PhotonNetwork.room == null)
+        {
+            // Create Room
+            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+                PhotonNetwork.CreateRoom(roomName + Guid.NewGuid().ToString("N"), new RoomOptions() { MaxPlayers = NetworkController.MAX_PLAYERS }, null);
+
+            // Join Room
+            if (roomsList != null)
+            {
+                for (int i = 0; i <= roomsList.Length; i++)
+            {
+                    if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].Name))
+                        PhotonNetwork.JoinRoom(roomsList[i].Name);
+                }
+            }
+        }
+    }
+
+    void OnReceivedRoomListUpdate()
+    {
+        roomsList = PhotonNetwork.GetRoomList();
+    }
+    void OnJoinedRoom()
+    {
+        Debug.Log("Connected to Room");
+        PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(UnityEngine.Random.value * 5, 1, 0) , Quaternion.identity, 0);
+    }
+}
---
+++ Escape Room/Assets/Escape Room/Scripts/Multiplayer/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 10f;
+    public GameObject playerPrefab;
+
+    void Update()
+    {
+        InputMovement();
+    }
+
+    void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.right * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.right * speed * Time.deltaTime);
+    }
+}
