9ae6be8e62290db04a9e1053d8298792a2f5f527
Patrick <patrickgeyer94@gmx.de>
2017-05-10 15:29:08 +0200

    undo a change

--- Escape Room/Assets/Escape Room/Scripts/Multiplayer/NetworkManager.cs
+++ Escape Room/Assets/Escape Room/Scripts/Multiplayer/NetworkManager.cs
@@ -16,49 +16,10 @@ public class NetworkManager : MonoBehaviour {
     }
 
 
-    private const string roomName = "RoomName";
-    private RoomInfo[] roomsList;
-
-    void OnGUI()
-    {
-        if (!PhotonNetwork.connected)
-        {
-            GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
-        }
-        else if (PhotonNetwork.room == null)
-        {
-            // Create Room
-            if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
-                PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 4 }, null);
-
-            // Join Room
-            if (roomsList != null)
-            {
-                for (int i = 0; i <= roomsList.Length; i++)
-            {
-                    if (GUI.Button(new Rect(100, 250 + (110 * i), 250, 100), "Join " + roomsList[i].Name))
-                        PhotonNetwork.JoinRoom(roomsList[i].Name);
-                }
-            }
-        }
-    }
-
-    void OnReceivedRoomListUpdate()
-    {
-        roomsList = PhotonNetwork.GetRoomList();
-    }
-    void OnJoinedRoom()
-    {
-        Debug.Log("Connected to Room");
-
-        GameObject player = PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(UnityEngine.Random.value * 5, 1, 0), Quaternion.identity, 0);
-    }
-
-
     // below, we implement some callbacks of PUN
     // you can find PUN's callbacks in the class PunBehaviour or in enum PhotonNetworkingMessage
 
-    /*
+
     public virtual void OnConnectedToMaster()
     {
         Debug.Log("OnConnectedToMaster() was called by PUN. Now this client is connected and could join a room. Calling: PhotonNetwork.JoinRandomRoom();");
@@ -88,5 +49,5 @@ public class NetworkManager : MonoBehaviour {
     {
         Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room. From here on, your game would be running. For reference, all callbacks are listed in enum: PhotonNetworkingMessage");
         GameObject player = PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(UnityEngine.Random.value*5, 1,  0), Quaternion.identity, 0);
-    }*/
+    }
 }
