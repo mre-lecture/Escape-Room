f6c8e4b236fbf350515dcccbbebd61667a92c324
Patrick <patrickgeyer94@gmx.de>
2017-05-10 13:41:06 +0200

    maybe fixe multiplayer mulity steuerung

--- Escape Room/Assets/Escape Room/Scripts/Multiplayer/Player.cs
+++ Escape Room/Assets/Escape Room/Scripts/Multiplayer/Player.cs
@@ -8,7 +8,8 @@ public class Player : MonoBehaviour {
 
     void Update()
     {
-        InputMovement();
+        if (photonView.isMine)
+            InputMovement();
     }
 
     void InputMovement()
