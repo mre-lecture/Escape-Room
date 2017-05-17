f6206b4d28a9b28c84e8e828cbcff8aa687c1f24
DESKTOP-04VE4OR\VR_BOX_01 <test@test.test.de>
2017-05-17 16:56:31 +0200

    save sound thingies

---
+++ Escape Room/Assets/soundMaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class soundMaster : MonoBehaviour {
+
+    public AudioClip[] sounds;
+
+    private AudioSource audio;   
+
+
+
+    // Use this for initialization
+    void Start () {        
+        audio = GetComponent<AudioSource>();
+        audio.clip = sounds[Random.Range(0, sounds.Length)];
+        audio.Play();
+
+
+
+    }
+
+
+    void playRandomSound()
+    {
+        Invoke("LaunchProjectile", 2);
+    }
+
+
+}
