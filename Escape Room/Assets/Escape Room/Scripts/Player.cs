c5e8fc776c6679b95b5ebc244d9ba019594d2b04
kshits <katharina_s15@hotmail.de>
2017-05-18 01:21:14 +0200

    added robot player to the raetselraum

---
+++ Escape Room/Assets/Escape Room/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponentInChildren<Animator>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("up")) {
+            anim.SetInteger("AnimPar", 1);
+        }
+        else if(Input.GetKey("right"))
+        {
+            anim.SetInteger("AnimPar", 2);
+        }
+        else if (Input.GetKey("left"))
+        {
+            anim.SetInteger("AnimPar", 3);
+        }
+        else
+        {
+            anim.SetInteger("AnimPar", 0);
+        }
+	}
+}
