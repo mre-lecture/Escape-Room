921bc32cf20a1dd6bc71ed45d3659137e39a37f2
kshits <katharina_s15@hotmail.de>
2017-05-23 00:11:14 +0200

    Added Player visualization; torch Rotation; moved Rainer

---
+++ Escape Room/Assets/Escape Room/Scripts/torchRotation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class torchRotation : MonoBehaviour
+{
+
+    public Animator animator;
+
+    GameObject door = GameObject.Find("Gate_door");
+    GameObject torch = GameObject.Find("Torch3");
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.enabled = true;
+         
+
+    }
+
+    void Update()
+    {
+       /* if (torchPar >= 70)
+        {
+            animator.enabled = true;
+            animator.Play("Key_OpenDoorAnimation");
+
+            door.GetComponent<BoxCollider>().enabled = true;
+        }
+*/        
+
+    }
+}
