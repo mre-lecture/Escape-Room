e50cf91dc47751b8a145c09ecfcb4c43863760e0
Patrick <patrickgeyer94@gmx.de>
2017-05-23 16:30:45 +0200

    Added candle physics and teleport areas

--- Escape Room/Assets/TeleportManagerScript.cs
+++ Escape Room/Assets/TeleportManagerScript.cs
@@ -8,7 +8,9 @@ public class TeleportManagerScript : Photon.MonoBehaviour {
     [SerializeField]
     private TeleportArea teleportAreaCell;
     [SerializeField]
-    private TeleportArea teleportAreaRoom;
+    private TeleportArea teleportAreaRoom1;
+    [SerializeField]
+    private TeleportArea teleportAreaRoom2;
 
     //Used to initialize the teleport settings correct for each player
     void OnJoinedRoom()
@@ -17,8 +19,9 @@ public class TeleportManagerScript : Photon.MonoBehaviour {
 		if (PhotonNetwork.player.IsMasterClient ) {                        
 			teleportAreaCell.locked = true;
 		} else {
-			teleportAreaRoom.locked = true;
-		}
+			teleportAreaRoom1.locked = true;
+            teleportAreaRoom2.locked = true;
+        }
 	}
 
 	//just for test purposes 
