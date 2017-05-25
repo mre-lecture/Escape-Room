1a23343a3a918c4c0f4e6ab1bfe7a10aa06f4ea3
RicoZieger <rico.zieger@gmail.com>
2017-05-26 00:05:13 +0200

    Added hidden door (+script) into the test scene.

--- Escape Room/Assets/CollisionTrigger.cs
+++ Escape Room/Assets/CollisionTrigger.cs
@@ -2,23 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 namespace Valve.VR.InteractionSystem{
 	
 	public class CollisionTrigger : MonoBehaviour {
 
 		[SerializeField]
 		private GameObject fittingObject;
-		private Throwable throwable;
+
+		[SerializeField]
+		private Vector3 finalPosition = new Vector3 (0, 0, 0);
+
+		[SerializeField]
+		private Vector3 finalRotation = new Vector3 (0, 0, 0);
+
+		[SerializeField]
+		private GameObject hiddenDoor;
 
 		void OnTriggerEnter(Collider collider){
-			if (collider.gameObject == fittingObject) {
-				throwable = collider.gameObject.GetComponent<Throwable> ();
-				//TODO detach from hand first ! Also make sure it does not fall through the wall or so
-				Destroy (throwable);
-			} else {
-				Debug.Log ("DOES NOT FIT");
-			}
+			if (collider.gameObject == fittingObject) {	
+				//detach from hand
+				collider.gameObject.transform.parent = null;
+				//remove throwable script
+				//TODO this prevents the hidden cube to become throwable !!!
+				//-->Steam throwable script still seems to have locked the hand!
+				Destroy (collider.gameObject.GetComponent<Throwable> ());
+				//set final position of object
+				collider.gameObject.transform.position = finalPosition;
+				//set final rotation of object
+				collider.gameObject.transform.eulerAngles = finalRotation;
+				//fire events of corresponding hidden door that might by unlocked now
+				hiddenDoor.GetComponent<HiddenDoor>().fireEvent();
+			} 
 		}
 	}
 
---
+++ Escape Room/Assets/HiddenDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem{
+	
+	public class HiddenDoor : MonoBehaviour {
+
+		[SerializeField]
+		private GameObject hiddenContent;
+
+		public int eventsRequiredToOpenDoor = 1;
+		private int eventsFired = 0;
+
+		public void fireEvent(){
+			if (++eventsFired == eventsRequiredToOpenDoor) {
+				transform.Rotate(Vector3.right * 90);
+				hiddenContent.AddComponent<Throwable>();
+			}
+		}
+	}
+
+}
