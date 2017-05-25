080e6e911070cc28423202dbc5dd38a6b703154b
RicoZieger <rico.zieger@gmail.com>
2017-05-25 12:29:50 +0200

    Added new scene and scripts to test the collision of objects for my rätsel :D

---
+++ Escape Room/Assets/CollisionTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Valve.VR.InteractionSystem{
+	
+	public class CollisionTrigger : MonoBehaviour {
+
+		[SerializeField]
+		private GameObject fittingObject;
+		private Throwable throwable;
+
+		void OnTriggerEnter(Collider collider){
+			if (collider.gameObject == fittingObject) {
+				throwable = collider.gameObject.GetComponent<Throwable> ();
+				//TODO detach from hand first ! Also make sure it does not fall through the wall or so
+				Destroy (throwable);
+			} else {
+				Debug.Log ("DOES NOT FIT");
+			}
+		}
+	}
+
+}
