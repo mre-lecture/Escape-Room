00438a90e722658e66805a5069433db1c13a0c01
RicoZieger <rico.zieger@gmail.com>
2017-05-27 00:25:00 +0200

    Added new scene to test the torch rätsel mechanic. Works so far.
    Note that torch audio is not handled in this scene.

---
+++ Escape Room/Assets/DoorRequirements.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem{
+
+	public class DoorRequirements : MonoBehaviour {
+
+		[SerializeField]
+		private GameObject[] requiredLights;
+
+		private bool unlocked;
+		
+		// Update is called once per frame
+		void Update () {
+			if (!unlocked) {
+				unlocked = true;
+				foreach (GameObject go in requiredLights) {
+					if (!go.GetComponent<Light> ().enabled)
+						unlocked = false;
+				}
+				if (unlocked) {
+					GetComponent<CircularDrive> ().maxAngle = 0;
+				}
+			}
+		}
+	}
+
+}
---
+++ Escape Room/Assets/torchRequirements.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem{
+
+	public class torchRequirements : MonoBehaviour {
+
+		[SerializeField]
+		private GameObject[] requiredTorches;
+
+		[SerializeField]
+		private GameObject light;
+
+		[SerializeField]
+		private GameObject flame;
+
+		private bool unlocked;
+
+		void Update(){
+			if (!unlocked) {				
+				unlocked = true;
+				foreach (GameObject go in requiredTorches) {					
+					if (go.transform.localRotation.z < .17f)
+						unlocked = false;					
+				}
+				if (unlocked) {					
+					light.GetComponent<Light> ().enabled = true;
+					flame.GetComponent<ParticleSystem> ().Play ();
+				}
+			}
+		}
+	}
+
+}
