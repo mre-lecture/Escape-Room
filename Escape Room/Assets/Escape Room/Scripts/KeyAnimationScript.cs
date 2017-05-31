75fc5642e7f3b6336df9ca09cfa8ac5394eeedf5
Steffen <s.brehm1@gmx.de>
2017-05-31 03:28:08 +0200

    Added compass puzzle and improved key-door opening

--- Escape Room/Assets/Escape Room/Scripts/KeyAnimationScript.cs
+++ Escape Room/Assets/Escape Room/Scripts/KeyAnimationScript.cs
@@ -7,31 +7,44 @@ public class KeyAnimationScript : MonoBehaviour
 {
 
     public Animator animator;
+    public GameObject unlockableDoor;
 
     // Use this for initialization
     void Start()
     {
         animator = GetComponent<Animator>();
         animator.enabled = false;
-        //GameObject door = GameObject.Find("Gate_door");
-        //door.GetComponent<Interactable>().enabled = false;
     }
 
+    
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag.Equals("KeyTrigger"))
         {
             animator.enabled = true;
-            animator.Play("Key_OpenDoorAnimation");
+
+            //Should be done right before destroy, but don`t know how to
+            //we need to do this, before this object is destroyed!
+            UnlockDoor();
+
+            //Destroy this this object after the animation is finished
+            Destroy(gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
             
-            
-            GameObject door = GameObject.Find("Gate_door");
-            door.GetComponent<BoxCollider>().enabled = true;
+
+            //detach from hand
+            this.gameObject.transform.parent = null;
+            //detach object from hand (depending on which hand it is in)
+            Hand[] hands = GetComponents<Hand>();
+            foreach (Hand hand in hands)
+            {
+                hand.DetachObject(collider.gameObject, true);
+            }
         }
     }
 
-    void DestroyKey()
+
+    private void UnlockDoor()
     {
-        Destroy(this.gameObject);
+        unlockableDoor.GetComponent<CircularDrive>().maxAngle = 204;
     }
 }
---
+++ Escape Room/Assets/Escape Room/Scripts/WallCompassPuzzle.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+using Valve.VR.InteractionSystem;
+
+
+public class WallCompassPuzzle : MonoBehaviour {
+
+
+    public enum CompassDirection { North, East, South, West };
+    public CompassDirection[] directionOrder;
+    public GameObject unlockableFlap;
+    public GameObject[] unlockableItems;
+
+    private int progress = 0;
+
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        if (hand.GetStandardInteractionButtonUp())
+        {
+            applyDirection();
+        }
+    }
+
+
+    void applyDirection()
+    {
+        CompassDirection direction = GetCurrentCompassDirection();
+        
+
+        if (directionOrder[progress] == direction)
+        {
+            progress++;
+        }
+        else
+        {
+            if (directionOrder[0] == direction)
+            {
+                progress = 1;
+            }
+            else
+            {
+                progress = 0;
+            }
+        }
+
+
+        if (progress >= directionOrder.Length)
+        {
+            //puzzle solved -> open flap
+            openFlap();
+            addScriptsToUnlockedItems();
+
+            //script isn`t needed anymore -> destroy
+            Destroy(this);
+        }
+    }
+
+
+    private CompassDirection GetCurrentCompassDirection()
+    {
+        CompassDirection direction;
+        float rotation = this.gameObject.GetComponent<LinearMapping>().value;
+        int section = (int)Math.Round(rotation / 0.25);
+
+
+        switch (section)
+        {
+            case 0:
+            case 4:
+                direction = CompassDirection.North;
+                break;
+            case 1:
+                direction = CompassDirection.East;
+                break;
+            case 2:
+                direction = CompassDirection.South;
+                break;
+            case 3:
+                direction = CompassDirection.West;
+                break;
+            default:
+                Debug.LogError("Unknown direction. Rotation was: " + rotation + ". Section was: " + section);
+                direction = CompassDirection.North;
+                break;
+        }
+
+        return direction;
+    }
+
+
+    private void openFlap()
+    {
+        unlockableFlap.transform.Rotate(Vector3.forward * 90);
+    }
+
+
+    private void addScriptsToUnlockedItems()
+    {
+        foreach (GameObject unlockableItem in unlockableItems)
+        {
+            unlockableItem.AddComponent<Interactable>();
+            unlockableItem.AddComponent<Throwable>();
+            //manually instantiate both unity events because of some weird bug they are
+            //sometimes not automatically added. That causes the throwable script not to work properly
+            unlockableItem.GetComponent<Throwable>().onPickUp = new UnityEngine.Events.UnityEvent();
+            unlockableItem.GetComponent<Throwable>().onDetachFromHand = new UnityEngine.Events.UnityEvent();
+        }
+    }
+}
