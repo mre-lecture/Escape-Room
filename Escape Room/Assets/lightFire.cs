96abdad9392a3bcf26a95135fea3ba80d94479b7
RicoZieger <rico.zieger@gmail.com>
2017-06-04 17:42:22 +0200

    Added first test to light a torch in the forge

---
+++ Escape Room/Assets/lightFire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class lightFire : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject torchHead;
+
+	[SerializeField]
+	private GameObject fireSource;
+
+	void OnTriggerEnter(Collider collider){		
+		if (collider.gameObject == torchHead) {				
+			fireSource.GetComponent<FireSource> ().isBurning = true;
+			fireSource.GetComponent<FireSource> ().burnTime = 100;
+		}
+	}
+}
