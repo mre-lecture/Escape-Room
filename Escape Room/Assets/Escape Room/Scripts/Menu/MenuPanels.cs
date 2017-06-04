060183405de9a6d4455b6d8db4f31c88ca46c6ca
Patrick <patrickgeyer94@gmx.de>
2017-06-04 16:28:15 +0200

    Try to fix door sync and add first VR menu to join room

---
+++ Escape Room/Assets/Escape Room/Scripts/Menu/MenuPanels.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPanels : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ConnectedView;
+    [SerializeField]
+    private GameObject ConnectingView;
+
+    protected virtual void Update()
+    {
+        if (PhotonNetwork.connected)
+        {
+            ShowConnectedGUI();
+        }
+        else
+        {
+            ShowConnectingGUI();
+        }
+    }
+
+    protected void ShowConnectingGUI()
+    {
+        ConnectedView.SetActive(false);
+        ConnectingView.SetActive(true);
+    }
+
+    protected void ShowConnectedGUI()
+    {
+        ConnectedView.SetActive(true);
+        ConnectingView.SetActive(false);
+    }
+}
---
+++ Escape Room/Assets/Escape Room/Scripts/Menu/MenuPlayerNameKeyboard.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuPlayerNameKeyboard : MenuPanels
+{
+    [SerializeField]
+    private Text playerNameTextField;
+    [SerializeField]
+    private GameObject bigGrid;
+    [SerializeField]
+    private GameObject smallGrid;
+
+    public static string playerNamePrefKey = "PlayerName";
+
+
+    void Start()
+    {
+        string defaultName = "Spieler Name";
+
+        if (playerNameTextField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                playerNameTextField.text = defaultName;
+            }
+            else
+            {
+                PlayerPrefs.SetString(playerNamePrefKey, defaultName);
+            }
+        }
+
+        PhotonNetwork.playerName = defaultName;
+    }
+
+    public void SetPlayerName()
+    {
+        if (!String.IsNullOrEmpty(playerNameTextField.text))
+        {
+            PhotonNetwork.playerName = playerNameTextField.text;
+
+            PlayerPrefs.SetString(playerNamePrefKey, playerNameTextField.text);
+        }
+        else
+        {
+            playerNameTextField.text = PlayerPrefs.GetString(playerNamePrefKey);
+        }
+    }
+    
+    public void AddCharToNameText(Text textToAdd)
+    {
+        playerNameTextField.text += textToAdd.text;
+    }
+
+    public void ShiftButtonPressed()
+    {
+        bigGrid.SetActive(!bigGrid.activeSelf);
+        smallGrid.SetActive(!smallGrid.activeSelf);
+    }
+
+    public void OkButtonPressed()
+    {
+        SetPlayerName();
+    }
+
+    public void SpaceButtonPressed()
+    {
+        playerNameTextField.text += " ";
+    }
+
+    public void BackButtonPressed()
+    {
+        if (!String.IsNullOrEmpty(playerNameTextField.text))
+        {
+            playerNameTextField.text = playerNameTextField.text.Remove(playerNameTextField.text.Length - 1);
+        }
+    }
+
+    public void DeleteButtonPressed()
+    {
+        playerNameTextField.text = String.Empty;
+    }
+}
