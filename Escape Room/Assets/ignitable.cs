0b97c3a8d1099c0a85d7fcb7df0ac701107645ba
RicoZieger <rico.zieger@gmail.com>
2017-06-05 12:46:28 +0200

    Added cage with wodden sticks as fence so that they have to be burned to open the cage

---
+++ Escape Room/Assets/ignitable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ignitable : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject ignitionObject;
+
+	[SerializeField]
+	private GameObject ignitionObjectFlame;
+
+	[SerializeField]
+	private GameObject flame;
+
+	private bool alreadyActive;
+
+	void OnTriggerEnter(Collider collider){	
+		if (collider.gameObject == ignitionObject && !alreadyActive && ignitionObjectFlame.GetComponent<ParticleSystem>().isPlaying) {
+			alreadyActive = true;
+			GetComponent<Light> ().enabled = true;
+			flame.GetComponent<ParticleSystem> ().Play ();
+			Invoke ("destroyAfterBurnedDown", 20);
+		}
+	}
+
+	private void destroyAfterBurnedDown(){
+		Destroy (gameObject);
+		flame.GetComponent<ParticleSystem> ().Stop ();
+		GetComponent<Light> ().enabled = false;
+	}
+}
