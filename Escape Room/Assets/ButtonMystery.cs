82d26d76a1d6a28922a9d36e5a7a94c1e81138f0
kshits <katharina_s15@hotmail.de>
2017-06-05 15:05:49 +0200

    changes on buttonsbehavior on mysterybuttons

---
+++ Escape Room/Assets/ButtonMystery.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMystery : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject button1;
+
+    [SerializeField]
+    private GameObject button2;
+
+    [SerializeField]
+    private GameObject button3;
+
+    [SerializeField]
+    private GameObject button4;
+
+    [SerializeField]
+    private GameObject button5;
+
+    [SerializeField]
+    private GameObject button6;
+
+    [SerializeField]
+    private GameObject button7;
+
+    [SerializeField]
+    private GameObject button8;
+
+    [SerializeField]
+    private GameObject button9;
+
+    [SerializeField]
+    private GameObject hiddenDoor;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+         if ((button1.GetActive()  && button4.GetActive()  && button8.GetActive())  && !(button2.GetActive() || button3.GetActive() ||  button5.GetActive()
+            || button6.GetActive() || button7.GetActive() ||  button9.GetActive()))
+         {
+             Destroy(hiddenDoor);
+         }
+    }
+    
+}
--- Escape Room/Assets/Escape Room/Scripts/ButtonsBehavior.cs
+++ Escape Room/Assets/Escape Room/Scripts/ButtonsBehavior.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR.InteractionSystem;
@@ -6,27 +7,19 @@ using Valve.VR.InteractionSystem;
 public class ButtonsBehavior : MonoBehaviour {
     public const float DISTANCE = 0.05f;
     private bool pressed = false;
-    public Hand.AttachmentFlags pressingGripPoint = Hand.AttachmentFlags.ParentToHand | Hand.AttachmentFlags.DetachFromOtherHand;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	void ViveGripInteractionStart(Hand.AttachmentFlags gripPoint)
+    [SerializeField]
+    private GameObject activeButton;
+   
+
+    private void OnTriggerEnter(Collider collider)
     {
-        if(pressed) { return; }
+        if (pressed) { return; }
 
-        transform.position += new Vector3(0, 0, DISTANCE);
-        pressed = true;
-        pressingGripPoint = gripPoint;
-    }
-
-    void ViveGripInteractionStop(Hand.AttachmentFlags gripPoint)
-    {
-        if (!pressed ) { return; }
-
-        transform.position += new Vector3(0, 0, DISTANCE);
-        pressed = false;
+     
+            activeButton.transform.position += new Vector3(0, 0, DISTANCE);
+            activeButton.SetActive(true);
+        
+        
     }
 }
