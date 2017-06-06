c0df245dafa50fa2e6521af630dd570a1fe640a4
RicoZieger <rico.zieger@gmail.com>
2017-06-07 00:03:59 +0200

    Implemented both table puzzles
    *both are available as entire prefabs (moveableTable, kingArtusTable)
    *already added some textures and models (table, knight model)
    
    Note that multiplayer scripts (Sync Moveable/Throwable Objects)
    have to be added if needed !

---
+++ Escape Room/Assets/KnightFigurePlace.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class KnightFigurePlace : MonoBehaviour {
+
+	[SerializeField]
+	private int KnightYRotation = 0;
+
+	[SerializeField]
+	private GameObject hand1,hand2,fallbackHand;
+
+	[SerializeField]
+	private artusSword swordScript;
+
+	private bool holdsFigure;
+
+	void OnTriggerEnter(Collider collider){		
+		if (collider.gameObject.tag == "Knight" && !holdsFigure) {	
+			//disable this spot for other figures
+			holdsFigure = true;
+			//detach from hand
+			collider.gameObject.transform.parent = null;
+			//detach object from hand (depending on which hand it is in)
+			hand1.GetComponent<Hand>().DetachObject(collider.gameObject,true);
+			hand2.GetComponent<Hand>().DetachObject(collider.gameObject,true);
+			fallbackHand.GetComponent<Hand>().DetachObject(collider.gameObject,true);
+			//remove throwable script
+			Destroy (collider.gameObject.GetComponent<Throwable>());
+			Destroy (collider.gameObject.GetComponent<Rigidbody>());
+			//add knight to hierarchy
+			collider.gameObject.transform.SetParent(gameObject.transform);
+			//set knight position
+			collider.gameObject.transform.localPosition = new Vector3(0,0,0);
+			//set knight rotation
+			collider.gameObject.transform.eulerAngles = new Vector3(270,KnightYRotation,0);
+			//update sword
+			swordScript.figureAdded();
+		}
+	}
+}
---
+++ Escape Room/Assets/artusSword.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class artusSword : MonoBehaviour {
+
+	[SerializeField]
+	private int amountFiguresNeedes = 4;
+
+	public void figureAdded(){
+		if (--amountFiguresNeedes == 0) {
+			//make sword throwable (it already is interactable aso.)
+			gameObject.AddComponent<Throwable> ();
+			//manually instantiate both unity events because of some weird bug they are
+			//sometimes not automatically added. That causes the throwable script not to work properly
+			gameObject.GetComponent<Throwable> ().onPickUp = new UnityEngine.Events.UnityEvent ();
+			gameObject.GetComponent<Throwable> ().onDetachFromHand = new UnityEngine.Events.UnityEvent ();
+		}
+	}
+}
