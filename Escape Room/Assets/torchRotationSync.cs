77492e60761d20154ca48a9f4003f2fba172c46e
DESKTOP-04VE4OR\VR_BOX_01 <test@test.test.de>
2017-06-06 16:21:43 +0200

    added full multiplayer sync functionality to scene

---
+++ Escape Room/Assets/torchRotationSync.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class torchRotationSync : MonoBehaviour
+{  
+    void HandHoverUpdate(Hand hand)
+    {        
+        if (hand.GetStandardInteractionButtonUp())
+        {
+            GetComponent<CircularDrive>().outAngle = 0;
+            //TODO: Adjust rotation to the scene
+            gameObject.transform.eulerAngles = new Vector3(0,180,0);
+        } 
+    }    
+
+}
