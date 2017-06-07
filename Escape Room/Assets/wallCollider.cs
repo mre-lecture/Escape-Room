9e714dad013e87dffe998fbb9e482d7de27f3c0f
RicoZieger <rico.zieger@gmail.com>
2017-06-08 00:45:38 +0200

    First tries to make grabbed objects collide with walls. Works meh.
    If someone is interested, it is added to the cube in the FackelnGang scene.

---
+++ Escape Room/Assets/wallCollider.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class wallCollider : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject hand1,hand2,fallbackHand;
+
+	private bool onHand,resetCubeDueToCollision;
+	private bool validCollision;
+
+	private float oldPositionX, oldPositionY, oldPositionZ, newPositionX, newPositionY, newPositionZ;
+	private float oldRotationX, oldRotationY, oldRotationZ, newRotationX, newRotationY, newRotationZ;
+
+
+	private void HandAttachedUpdate( Hand hand )
+	{		
+		if (gameObject.transform.hasChanged) {			
+			oldPositionX = newPositionX;
+			oldPositionY = newPositionY;
+			oldPositionZ = newPositionZ;
+			oldRotationX = newRotationX;
+			oldRotationY = newRotationY;
+			oldRotationZ = newRotationZ;
+
+			newPositionX = gameObject.transform.position.x;
+			newPositionY = gameObject.transform.position.y;
+			newPositionZ = gameObject.transform.position.z;
+			newRotationX = gameObject.transform.eulerAngles.x;
+			newRotationY = gameObject.transform.eulerAngles.y;
+			newRotationZ = gameObject.transform.eulerAngles.z;
+
+			gameObject.transform.hasChanged = false;
+		} 
+	}
+
+	private void OnAttachedToHand( Hand hand )
+	{
+		onHand = true;			
+	}
+
+	private void OnDetachedFromHand( Hand hand )
+	{
+		onHand = false;		
+	}
+
+	private void OnTriggerEnter(Collider collider)
+	{
+		if (onHand && collider.gameObject.tag.Equals("collidingTexture")) {
+			resetCubeDueToCollision = true;
+			//GetComponent<Throwable> ().hack = true;
+			gameObject.transform.parent = null;
+		}
+	}
+
+	private void OnTriggerExit(Collider collider){
+		if (onHand && collider.gameObject.tag.Equals("collidingTexture")) {
+			resetCubeDueToCollision = false;
+			//GetComponent<Throwable> ().hack = false;
+		}
+	}
+
+	private IEnumerator handleCollision(){
+		yield return new WaitForEndOfFrame();
+		Debug.Log ("RESET");
+
+
+		gameObject.transform.position = new Vector3 (oldPositionX, oldPositionY, oldPositionZ);
+		gameObject.transform.eulerAngles = new Vector3 (oldRotationX, oldRotationY, oldRotationZ);
+
+		newPositionX = oldPositionX;
+		newPositionY = oldPositionY;
+		newPositionZ = oldPositionZ;
+		newRotationX = oldRotationX;
+		newRotationY = oldRotationY;
+		newRotationZ = oldRotationZ;
+
+		/*
+		//detach from hand
+		gameObject.transform.parent = null;
+		//detach object from hand (depending on which hand it is in)
+		hand1.GetComponent<Hand>().DetachObject(gameObject,true);
+		hand2.GetComponent<Hand>().DetachObject(gameObject,true);
+		fallbackHand.GetComponent<Hand>().DetachObject(gameObject,true);
+
+		GetComponent<Throwable> ().onPickUp = new UnityEngine.Events.UnityEvent ();
+		GetComponent<Throwable> ().onDetachFromHand = new UnityEngine.Events.UnityEvent ();
+		*/
+	}
+
+	void Update(){
+		//TODO: Die Funktion HandAttachedUpdate in Throwable konkuriert mit dieser Funktion, da sie die akutelle Position immer auf die Hand setzt.
+		//TODO: ne das alleine wars nicht (hab zwischenzeitlich den bool hack eingeführt)...vllt weils ein child von der hand ist?
+		//TODO: besser, aber auch noch nicht die ultimative Lösung
+		if (resetCubeDueToCollision) {
+			gameObject.transform.position = new Vector3 (oldPositionX, oldPositionY, oldPositionZ);
+			gameObject.transform.eulerAngles = new Vector3 (oldRotationX, oldRotationY, oldRotationZ);
+		}
+	}
+
+
+}
