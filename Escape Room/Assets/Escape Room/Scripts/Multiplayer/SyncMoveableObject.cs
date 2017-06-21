5b8dc5cbb2c43f078b53410b6d6fb53f6673cec6
Patrick <patrickgeyer94@gmx.de>
2017-06-21 16:05:05 +0200

    correct syncmoveableobjcets

--- Escape Room/Assets/Escape Room/Scripts/Multiplayer/SyncMoveableObject.cs
+++ Escape Room/Assets/Escape Room/Scripts/Multiplayer/SyncMoveableObject.cs
@@ -6,12 +6,27 @@ using Valve.VR.InteractionSystem;
 public class SyncMoveableObject : SyncInteractableObjects
 {
 
+    private CircularDrive circularDrive;
+    private LinearMapping linearMapping;
+
+    private float correctOutAngle;
+    private float correctLinearMappingValue;
+
+
+    void Start()
+    {
+        circularDrive = GetComponent<CircularDrive>();
+        linearMapping = GetComponent<LinearMapping>();
+    }
+
     void Update()
     {
         if (!photonView.isMine)
         {
             transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 15);
             transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 15);
+            circularDrive.outAngle = correctOutAngle;
+            linearMapping.value = correctLinearMappingValue;
 
         }
     }
@@ -22,6 +37,27 @@ public class SyncMoveableObject : SyncInteractableObjects
         if (hand.GetStandardInteractionButtonDown())
         {
             GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.player.ID);
+        }
+    }
+
+    public new void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            // We own this player: send the others our data
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(circularDrive.outAngle);
+            stream.SendNext(linearMapping.value);
+        }
+        else
+        {
+            // Network player, receive data
+            correctPlayerPos = (Vector3)stream.ReceiveNext();
+            correctPlayerRot = (Quaternion)stream.ReceiveNext();
+            correctOutAngle = (float)stream.ReceiveNext();
+            correctLinearMappingValue = (float)stream.ReceiveNext();
+
         }
     }
 }
