bea67a1673d9575445789459784e2d4cf7ea6efd
Patrick <patrickgeyer94@gmx.de>
2017-06-21 21:34:12 +0200

    add playerSpawn in game sceene

---
+++ Escape Room/Assets/Escape Room/Scripts/Multiplayer/SetPlayerStartPosInGameScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPlayerStartPosInGameScene : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerSpawnerMaster;
+    [SerializeField]
+    private GameObject playerSpawnerSecondPlayer;
+
+	// Use this for initialization
+	void Start () {
+
+	    GameObject tempPlayer = GameObject.Find("VRPlayer");
+
+        if (PhotonNetwork.isMasterClient)
+        {
+            tempPlayer.transform.position = playerSpawnerMaster.transform.position;
+
+        }
+	    else
+	    {
+	        tempPlayer.transform.position = playerSpawnerSecondPlayer.transform.position;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
