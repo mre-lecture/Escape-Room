11fb3d9fd60c6625e19a7edbcdcd16d0132f5e4e
kshits <katharina_s15@hotmail.de>
2017-06-25 17:06:34 +0200

    added mystry buttons scripts

---
+++ Escape Room/Assets/Escape Room/Scripts/ButtonBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public const float DISTANCE = 0.05f;
+    public bool pressed = false;
+
+    [SerializeField]
+    private GameObject activeButton;
+
+    // For Mouse testing
+    private void OnMouseDown()
+    {
+        if (pressed)
+        {
+            activeButton.transform.position += new Vector3( DISTANCE, 0, 0);
+            pressed = false;
+            return;
+        }
+        else
+        {
+            activeButton.transform.position += new Vector3(-DISTANCE, 0, 0);
+            pressed = true;
+        }
+
+    }
+
+    /*private void HandHoverUpdate(Hand hand)
+    {
+        if (hand.GetStandardInteractionButtonDown())
+        {
+            pressButton();
+        }
+    }
+
+    // For Trigger Enter - Check Collider
+    private void pressButton()
+    {
+        if (pressed)
+        {
+            activeButton.transform.position += new Vector3(DISTANCE, 0, 0);
+            pressed = false;
+            return;
+        }
+        else
+        {
+            activeButton.transform.position += new Vector3(-DISTANCE, 0, 0);
+            pressed = true;
+        }
+
+    }
+    */
+}
--- Escape Room/Assets/Escape Room/Scripts/ButtonsBehavior.cs
+++ Escape Room/Assets/Escape Room/Scripts/ButtonsBehavior.cs
@@ -1,32 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Valve.VR.InteractionSystem;
 
-public class ButtonsBehavior : MonoBehaviour {
-    public const float DISTANCE = 0.05f;
-    private bool pressed = false;
-    public Hand.AttachmentFlags pressingGripPoint = Hand.AttachmentFlags.ParentToHand | Hand.AttachmentFlags.DetachFromOtherHand;
+public class ButtonsBehavior : MonoBehaviour
+{
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	void ViveGripInteractionStart(Hand.AttachmentFlags gripPoint)
+    [SerializeField]
+    private GameObject button1;
+
+    [SerializeField]
+    private GameObject button2;
+
+    [SerializeField]
+    private GameObject button3;
+
+    [SerializeField]
+    private GameObject button4;
+
+    [SerializeField]
+    private GameObject button5;
+
+    [SerializeField]
+    private GameObject button6;
+
+    [SerializeField]
+    private GameObject button7;
+
+    [SerializeField]
+    private GameObject button8;
+
+    [SerializeField]
+    private GameObject button9;
+
+    [SerializeField]
+    private GameObject hiddenDoor;
+
+
+    // Update is called once per frame
+
+    void Update()
     {
-        if(pressed) { return; }
+        ButtonBehavior button1Script = button1.GetComponent<ButtonBehavior>();
+        ButtonBehavior button2Script = button2.GetComponent<ButtonBehavior>();
+        ButtonBehavior button3Script = button3.GetComponent<ButtonBehavior>();
+        ButtonBehavior button4Script = button4.GetComponent<ButtonBehavior>();
+        ButtonBehavior button5Script = button5.GetComponent<ButtonBehavior>();
+        ButtonBehavior button6Script = button6.GetComponent<ButtonBehavior>();
+        ButtonBehavior button7Script = button7.GetComponent<ButtonBehavior>();
+        ButtonBehavior button8Script = button8.GetComponent<ButtonBehavior>();
+        ButtonBehavior button9Script = button9.GetComponent<ButtonBehavior>();
+/*
+        float button1Distance = button1.transform.position.z;
+        float button2Distance = button2.transform.position.z;
+        float button3Distance = button3.transform.position.z;
+        float button4Distance = button4.transform.position.z;
+        float button5Distance = button5.transform.position.z;
+        float button6Distance = button6.transform.position.z;
+        float button7Distance = button7.transform.position.z;
+        float button8Distance = button8.transform.position.z;
+        float button9Distance = button9.transform.position.z;*/
+        
+        if (button1Script.pressed && !button2Script.pressed && !button3Script.pressed && button4Script.pressed && !button5Script.pressed && 
+            !button6Script.pressed && !button7Script.pressed && button8Script.pressed && !button9Script.pressed)
 
-        transform.position += new Vector3(0, 0, DISTANCE);
-        pressed = true;
-        pressingGripPoint = gripPoint;
+        {
+            Destroy(button1.GetComponent<Rigidbody>());
+            Destroy(button1.GetComponent<BoxCollider>());
+
+            Destroy(button2.GetComponent<Rigidbody>());
+            Destroy(button2.GetComponent<BoxCollider>());
+
+            Destroy(button3.GetComponent<Rigidbody>());
+            Destroy(button3.GetComponent<BoxCollider>());
+
+            Destroy(button4.GetComponent<Rigidbody>());
+            Destroy(button4.GetComponent<BoxCollider>());
+
+            Destroy(button5.GetComponent<Rigidbody>());
+            Destroy(button5.GetComponent<BoxCollider>());
+
+            Destroy(button6.GetComponent<Rigidbody>());
+            Destroy(button6.GetComponent<BoxCollider>());
+
+            Destroy(button7.GetComponent<Rigidbody>());
+            Destroy(button7.GetComponent<BoxCollider>());
+
+            Destroy(button8.GetComponent<Rigidbody>());
+            Destroy(button8.GetComponent<BoxCollider>());
+
+            Destroy(button9.GetComponent<Rigidbody>());
+            Destroy(button9.GetComponent<BoxCollider>());
+
+            Destroy(hiddenDoor);
+        }
     }
 
-    void ViveGripInteractionStop(Hand.AttachmentFlags gripPoint)
-    {
-        if (!pressed ) { return; }
-
-        transform.position += new Vector3(0, 0, DISTANCE);
-        pressed = false;
-    }
 }
