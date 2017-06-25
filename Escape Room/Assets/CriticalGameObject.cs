fd29daa61a3d628af9077945a6669f9badb6a294
RicoZieger <rico.zieger@gmail.com>
2017-06-25 22:54:10 +0200

    Added simple object respawn script
    *Every GameObject that has this script as a component
    will be respawned at its original position in case it falls out of
    the level.
    Therefore a huge plane has to be placed below the level and
    the Tag "resetGround" has to be added to this plane. Furthermore
    it needs a trigger.

---
+++ Escape Room/Assets/CriticalGameObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CriticalGameObject : MonoBehaviour {
+
+	private Vector3 respawnPosition, respawnRotation;
+
+	void Awake(){
+		respawnPosition = gameObject.transform.position;
+		respawnRotation = gameObject.transform.eulerAngles;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "resetGround") {
+			gameObject.transform.position = respawnPosition;
+			gameObject.transform.eulerAngles = respawnRotation;
+		}
+	}
+}
