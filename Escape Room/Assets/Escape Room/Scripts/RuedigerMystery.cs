da46fa12927a96a91ace622f4e3e91fb72da2741
DESKTOP-04VE4OR\VR_BOX_01 <test@test.test.de>
2017-06-27 23:10:45 +0200

    add missing script

---
+++ Escape Room/Assets/Escape Room/Scripts/RuedigerMystery.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RuedigerMystery : Photon.MonoBehaviour {
+
+
+	public GameObject shieldTrigger, swordTrigger, helmTrigger;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (shieldTrigger.transform.childCount != 0 && swordTrigger.transform.childCount != 0 && helmTrigger.transform.childCount != 0) {
+			photonView.RPC("SwitchToCredits", PhotonTargets.All);
+		}
+	
+	}
+
+
+	[PunRPC]
+	public void SwitchToCredits()
+	{
+		SceneManager.LoadScene(3);
+	}
+}
