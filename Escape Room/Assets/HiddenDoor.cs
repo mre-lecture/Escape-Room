69043e5a109bf3ef1fb7cabe9b88db243bd8b08a
Patrick <patrickgeyer94@gmx.de>
2017-06-27 13:12:24 +0200

    completed aussparungen rätsel

--- Escape Room/Assets/HiddenDoor.cs
+++ Escape Room/Assets/HiddenDoor.cs
@@ -7,19 +7,23 @@ namespace Valve.VR.InteractionSystem{
 	public class HiddenDoor : MonoBehaviour {
 
 		[SerializeField]
-		private GameObject hiddenContent;
+		private GameObject[] hiddenContent;
 
 		public int eventsRequiredToOpenDoor = 1;
 		private int eventsFired = 0;
 
 		public void fireEvent(){
 			if (++eventsFired == eventsRequiredToOpenDoor) {
-				transform.Rotate(Vector3.right * 90);
-				hiddenContent.AddComponent<Throwable>();
-				//manually instantiate both unity events because of some weird bug they are
-				//sometimes not automatically added. That causes the throwable script not to work properly
-				hiddenContent.GetComponent<Throwable> ().onPickUp = new UnityEngine.Events.UnityEvent ();
-				hiddenContent.GetComponent<Throwable> ().onDetachFromHand = new UnityEngine.Events.UnityEvent ();
+				Destroy(gameObject);
+			    foreach (GameObject go in hiddenContent)
+			    {
+			        go.AddComponent<Throwable>();
+				    //manually instantiate both unity events because of some weird bug they are
+				    //sometimes not automatically added. That causes the throwable script not to work properly
+				    go.GetComponent<Throwable> ().onPickUp = new UnityEngine.Events.UnityEvent ();
+				    go.GetComponent<Throwable> ().onDetachFromHand = new UnityEngine.Events.UnityEvent ();
+			    }
+				
 			}
 		}
 	}
